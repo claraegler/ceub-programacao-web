fdc85e092a9297135996ebff2e798f2913b683b2
claraegler <claraeglerr@gmail.com>
2024-03-20 09:42:05 -0300

    alteracoes aula

--- script.js
+++ script.js
@@ -1,33 +1,7 @@
-/*
-let a = "5";
-let b = 2;
+let idade = 17;
+let nome = "joao";
+let altura = 1.80;
 
-console.log(typeof(a));
-console.log(typeof(b));
-*/
-
-// let numero1 = 0.1;
-// let numero2 = 0.2;
-
-// console.log(numero1 + numero2)
-
-let texto1 = "ola";
-let texto2 = "mundo";
-
-let resultadoTexto = (texto1 + ", " + texto2);
-
-console.log(resultadoTexto);
-
-/*
-let resultado = a - b;
-
-let resultado2 = a + b;
-
-console.log(resultado);
-console.log(resultado2);
-
-console.log(typeof(resultado));
-console.log(typeof(resultado2)); 
-*/
-
-
+if (idade >= 18 || nome != "pedro") {
+    console.log("ola!");
+}
