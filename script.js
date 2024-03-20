8d442e619aba9c01e106b05dfea0e8943dc45a37
claraegler <claraeglerr@gmail.com>
2024-03-20 09:05:19 -0300

    Alterações aula 20mar javascript

---
+++ script.js
@@ -0,0 +1,33 @@
+/*
+let a = "5";
+let b = 2;
+
+console.log(typeof(a));
+console.log(typeof(b));
+*/
+
+// let numero1 = 0.1;
+// let numero2 = 0.2;
+
+// console.log(numero1 + numero2)
+
+let texto1 = "ola";
+let texto2 = "mundo";
+
+let resultadoTexto = (texto1 + ", " + texto2);
+
+console.log(resultadoTexto);
+
+/*
+let resultado = a - b;
+
+let resultado2 = a + b;
+
+console.log(resultado);
+console.log(resultado2);
+
+console.log(typeof(resultado));
+console.log(typeof(resultado2)); 
+*/
+
+
