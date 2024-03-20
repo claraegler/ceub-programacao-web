b66690f2230a7581d404bbd6ba4009749cf4b171
claraegler <claraeglerr@gmail.com>
2024-03-20 10:15:12 -0300

    Alteracoes aula antes intervalo

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*
 let idade = 17;
 let nome = "joao";
 let altura = 1.80;
@@ -5,3 +6,19 @@ let altura = 1.80;
 if (idade >= 18 || nome != "pedro") {
     console.log("ola!");
 }
+
+let meuArray = [1, 3, 4, 6, 0, -3]
+
+console.log(meuArray[0]);
+console.log(meuArray);
+
+console.log(meuArray.length);
+
+let pessoa = {
+    nome: "joão",
+    idade: 18,
+    altura: 1.78
+}
+
+console.log( "a idade da pessoa " + pessoa.nome + " é " + pessoa.idade)
+*/
